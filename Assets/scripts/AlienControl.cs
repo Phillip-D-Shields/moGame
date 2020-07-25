8fd1a874e32fca7496dc3c0138c9214a0b649c2e
phillip <noReply@github>
2020-07-25 18:12:42 +1200

    basic setup, added pink alien with run and jump animations

---
+++ Assets/scripts/AlienControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienControl : MonoBehaviour
+{
+    private Rigidbody2D myRigidbody;
+    private Animator myAnimation;
+    public float jumpForce = 500f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimation = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Jump"))
+        {
+            myRigidbody.AddForce(transform.up * jumpForce);
+        }
+        myAnimation.SetFloat("vVelocity", myRigidbody.velocity.y);
+    }
+}
